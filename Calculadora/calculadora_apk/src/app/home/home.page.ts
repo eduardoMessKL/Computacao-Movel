c4d76f2c6141144bc02f534b6dec29d2ff4b161b
eduardoMessKL <eduardomk408@gmail.com>
2023-10-19 09:39:55 -0300

    Aula 19-10

--- Calculadora/calculadora_apk/src/app/home/home.page.ts
+++ Calculadora/calculadora_apk/src/app/home/home.page.ts
@@ -6,7 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor : number = 0;
+  visor : string = '0';
+  operacao! : number;
+  valor1! : number;
+  valor2! : number;
+
   constructor() {}
 
+  adicionarNumero(valor : string){
+    if((this.visor.length === 1) && (this.visor === '0')){
+      this.visor = valor;
+    } else {
+      this.visor += valor;
+    }
+  }
+
+  adicionarOperacao(valor : number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.zerar();
+    console.log(this.valor1);
+  }
+
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0 : { 
+        this.visor = "" + (this.valor1 + this.valor2); 
+      break;
+    }
+      case 1 : { 
+        this.visor = "" + (this.valor1 - this.valor2); 
+      break;
+    }
+      case 2 : { 
+        this.visor = "" + (this.valor1 * this.valor2); 
+      break;
+    }
+      case 3 : { 
+        this.visor = "" + (this.valor1 /  this.valor2); 
+      break;
+    }
+      case 4 : { 
+        this.visor = "" + (this.valor1 / 100); 
+      break;
+    }
+      case 5 : { 
+        this.visor = "" + ( - this.valor1); 
+      break;
+    }
+
+    }
+  }
+
+  zerar(){
+    this.visor = '0';
+  }
 }
